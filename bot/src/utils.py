9b427401697c8fe61bcdbe5019af80296575dc44
Nikita Silaev <Beeeirym@gmail.com>
2025-06-19 19:51:19 +0300

    feat: The respondEvent function now returns the bot's response message id

--- bot/src/utils.py
+++ bot/src/utils.py
@@ -3,14 +3,16 @@ from aiogram.types import Message, CallbackQuery
 import datetime
 
 
-async def respondEvent(event: Message | CallbackQuery, **kwargs):
+async def respondEvent(event: Message | CallbackQuery, **kwargs) -> int:
     "Responds to various types of events: messages and callback queries."
 
     if isinstance(event, Message):
-        await event.answer(**kwargs)
+        bot_message = await event.answer(**kwargs)
     elif isinstance(event, CallbackQuery):
-        await event.message.edit_text(**kwargs)
+        bot_message = await event.message.edit_text(**kwargs)
         await event.answer()
+
+    return bot_message.message_id
 
 
 def makeGreetingMessage() -> str:
